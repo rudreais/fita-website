5039a3242e9cbb262b8f62736d98031565722900
rudreais <valentinlattay@gmail.com>
2018-06-06 17:30:33 +0200

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="/index.php">
+	      	Home
+	      </a>
+	      <a class="navbar-brand" href="/commits.php">
+	      	Commits
+	      </a>
+	    </div>
+	  </div>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Home - FitA</title>
+</head>
+<?php include_once('includes/header.php'); ?>
+<body>
+<div class="container">
+	<div class="page-header">
+  		<h1>Fed in the Abyss <small>What is it about</small></h1>
+	</div>
+	<div class="page-header">
+  		<h1>Installation <small>Building and running</small></h1>
+	</div>
+	<div class="page-header">
+  		<h1>Usage <small>How to play</small></h1>
+	</div>
+	<div class="page-header">
+  		<h1>Table of Contents <small>Wiki (about the game)</small></h1>
+	</div>
+	<div class="page-header">
+  		<h1>Contributing <small>How can you help ?</small></h1>
+	</div>
+	<div class="page-header">
+  		<h1>Credits</h1>
+	</div>
+</div>
+</body>
+</html>
