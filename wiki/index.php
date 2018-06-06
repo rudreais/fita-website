bd86515919d2894edae0b87e7e79c34163f1992d
rudreais <valentinlattay@gmail.com>
2018-06-07 00:55:13 +0200

    added wiki page

---
+++ wiki/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Home - FitA</title>
+</head>
+<?php include_once('../includes/header.php'); ?>
+
+<body>
+<div class="container">
+<div class="page-header">
+  <h1>Todolist <small>wiki content</small></h1>
+</div>
+<ul class="list-group">
+    <li class="list-group-item">
+        races        
+    </li>
+    <li class="list-group-item">
+        classes
+    </li>
+    <li class="list-group-item">
+        weapons
+    </li>
+    <li class="list-group-item">
+        armors
+    </li>
+    <li class="list-group-item">
+        spells
+    </li>
+</ul>
+</div>
+</body>
+</html>
