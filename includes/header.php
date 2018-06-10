86f9703586911f9a69fc04e6f8db692bd95f26a3
rudreais <valentinlattay@gmail.com>
2018-06-10 19:07:00 +0200

    applying brand to header and favicon

--- includes/header.php
+++ includes/header.php
@@ -9,10 +9,10 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>    
+    <a class="navbar-brand" href="/"></a>
   </div>
   <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav">
-      <li><a href="/">Home</a></li>
       <li><a href="/commits.php">Commits</a></li>
       <li><a href="/wiki/code.php">Technical documentation</a></li>
     </ul>
